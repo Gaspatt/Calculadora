754e6be8b9970480442336c1f8fe4e86faa11fe4
Gaspatt <matheusgaspar29.if@gmail.com>
2023-11-22 09:12:11 -0300

    a

---
+++ Calculadora/script.js
@@ -0,0 +1,14 @@
+function Somar() {
+    let valor1 = document.querySelector('#valor1')
+    let valor2 = document.querySelector('#valor2')
+    let resultado = document.querySelector('#resultado')
+
+    let btsoma = document.querySelector('#btsoma')
+    btsoma.addEventListener('click', function () {
+        soma(Number(valor1.value), Number(valor2.value))
+    })
+
+    function soma(a, b) {
+        resultado.textContent - (a + b)
+    }
+}
